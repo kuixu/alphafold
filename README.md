e064f9b238e260d6345b1439016fd589fd1985da
Xu Kui <xukui.cs@gmail.com>
2021-07-18 10:31:51 +0800

    update readme

--- README.md
+++ README.md
@@ -25,11 +25,60 @@ The following steps are required in order to run AlphaFold:
 
 1.  Install softwares
     ```bash
+    
     git clone https://github.com/kuixu/alphafold.git
     cd alphafold
+    
+    ./install_on_ubuntu.sh
+    ```
+    or  step by step
+    ```
+
+    # work_path=/path/to/alphafold-code
+    work_path=$(PWD)
+    echo $work_path
+
+
+    # apt tools 
+    sudo apt-get install -y \
+        build-essential \
+        cmake \
+        git \
+        hmmer \
+        kalign \
+        tzdata \
+        wget \
+
+
+    # conda pkgs
     conda create -n af2 python=3.8 -y
     conda activate af2
-    ./install_on_ubuntu.sh
+
+    conda install -y -c conda-forge \
+        openmm=7.5.1 \
+        pdbfixer \
+        pip
+
+    # python pkgs
+    pip3 install --upgrade pip \
+        && pip3 install -r ./requirements.txt \
+        && pip3 install --upgrade "jax[cuda111]" -f \
+        https://storage.googleapis.com/jax-releases/jax_releases.html
+
+    # hh-suite
+    git clone --branch v3.3.0 https://github.com/soedinglab/hh-suite.git 
+    mkdir -p ./hh-suite/build 
+    cd ./hh-suite/build
+    cmake -DCMAKE_INSTALL_PREFIX=/opt/hhsuite .. \
+        && make -j 4 && sudo make install \
+        && sudo ln -s /opt/hhsuite/bin/* /usr/bin \
+    cd $work_path   
+
+    # update openmm 
+    a=$(which python)
+    cd $(dirname $(dirname $a))/lib/python3.8/site-packages
+    patch -p0 < $work_path/docker/openmm.patch
+
     ```
 
 1.  Download genetic databases (see below).
--- install_on_ubuntu.sh
+++ install_on_ubuntu.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-work_path=$(dirname $0)
 
+# work_path=/path/to/alphafold-code
+work_path=$(PWD)
+echo $work_path
+
+
+# apt tools 
 sudo apt-get install -y \
       build-essential \
       cmake \
@@ -10,6 +15,23 @@ sudo apt-get install -y \
       tzdata \
       wget \
 
+
+# conda pkgs
+conda create -n af2 python=3.8 -y
+conda activate af2
+
+conda install -y -c conda-forge \
+      openmm=7.5.1 \
+      pdbfixer \
+      pip
+
+# python pkgs
+pip3 install --upgrade pip \
+    && pip3 install -r ./requirements.txt \
+    && pip3 install --upgrade "jax[cuda111]" -f \
+      https://storage.googleapis.com/jax-releases/jax_releases.html
+
+# hh-suite
 git clone --branch v3.3.0 https://github.com/soedinglab/hh-suite.git 
 mkdir -p ./hh-suite/build 
 cd ./hh-suite/build
@@ -18,21 +40,8 @@ cmake -DCMAKE_INSTALL_PREFIX=/opt/hhsuite .. \
     && sudo ln -s /opt/hhsuite/bin/* /usr/bin \
 cd $work_path   
 
-conda install -y -c conda-forge \
-      openmm=7.5.1 \
-      pdbfixer \
-      pip
-
-wget -q -P ./alphafold/common/ \
-  https://git.scicore.unibas.ch/schwede/openstructure/-/raw/7102c63615b64735c4941278d92b554ec94415f8/modules/mol/alg/src/stereo_chemical_props.txt
-
-pip3 install --upgrade pip \
-    && pip3 install -r ./requirements.txt \
-    && pip3 install --upgrade "jax[cuda111]" -f \
-      https://storage.googleapis.com/jax-releases/jax_releases.html
-
+# update openmm 
 a=$(which python)
 cd $(dirname $(dirname $a))/lib/python3.8/site-packages
-# cd /opt/conda/lib/python3.8/site-packages
 patch -p0 < $work_path/docker/openmm.patch
 
